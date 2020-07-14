5571bdac8e2a12330128275837607d5cd0a4b41f
Kojixus <dkovi24@gmail.com>
2020-07-13 20:30:36 -0400

    Main Release v0.1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	window.SetVolume = function(val)
+	{
+    var player = document.getElementById('bg');
+    console.log('Before: ' + player.volume);
+    player.volume = val / 100;
+    console.log('After: ' + player.volume);
+	}
+	
+	});
